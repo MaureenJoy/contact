cffe4676c5809b39d693ef745f6e556917f6f024
MaureenJoy <73148238+MaureenJoy@users.noreply.github.com>
2020-12-06 12:09:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<?php include_once("index.html"); ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-witdh, initial scale=1">
+<title>Contact Us</title>
+<link rel="stylesheet" type="text/css" href="aboutstyle.css">
+
+</head>
+<body class="l">
+    <header >
+    <img class="logo" src="logo.png"   alt="logo">
+    <nav class="navbar">
+     <ul class="nav_links">
+            <li><a href="https://my117mj.herokuapp.com/" style="color: #000000">HOME</a>  </li>
+            <li><a href="product.html"style="color: #000000">PRODUCTS</a>  </li>
+            <li><a href="about.html" style="color: #000000">ABOUT</a>  </li>
+            <li><a href="contact.html" style="color: #000000">CONTACT US</a>    </li>
+          </ul>
+</nav>
+      <div class="search">
+       <button type="Submit">Search</button>
+        <input type="text" class="txt"  placeholder="&emsp;&emsp;Search" >
+</div>
+</header>
+<div class="img" >
+<h1>
+<br><br>
+	M:Customize your own candle
+	</h1>
+<p>
+
+	Address: Burgos,Ilocos,Norte Philippines 2918<br>
+	Facebook: M:CYOC<br>
+	Instagram: M:CYOC<br>
+	Email: m******c*******@gmail.com
+
+</p>
+</div>
+</body>
+</html>
